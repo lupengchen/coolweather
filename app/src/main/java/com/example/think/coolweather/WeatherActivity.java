0afad88b04a97d063027ea16e967895e1a01866c
Your Name <you@example.com>
2018-01-26 18:14:33 +0800

    增加后台自动更新天气功能。

--- app/src/main/java/com/example/think/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/think/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.think.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -23,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.think.coolweather.gson.Forecast;
 import com.example.think.coolweather.gson.Weather;
+import com.example.think.coolweather.service.AutoUpdateService;
 import com.example.think.coolweather.util.HttpUtil;
 import com.example.think.coolweather.util.Utility;
 
@@ -235,6 +237,9 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
 
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
 }
